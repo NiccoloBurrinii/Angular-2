5eb21759229a289b00af8107b00da65308cf1b30
lai2-24 <lai2-24@itismeucci.com>
2024-02-28 09:29:42 +0000

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Person } from './model/person';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-1';
+  buttonDisabled: boolean = false;
+  n = 0;
+
+  person: Person = new Person();
+
+  people: Person[] = []
+
+  addPerson = () => {
+  this.people.push(this.person);
+  this.n++;
+  }
 }
+
---
+++ src/app/model/person.ts
@@ -0,0 +1,11 @@
+export class Person {    //export = pubblica
+    name: string;
+    surname: string;
+    age: number;
+
+    constructor() {
+        this.name = "";
+        this.surname = "";
+        this.age = 0;
+    }
+}
